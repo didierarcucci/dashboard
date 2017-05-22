49efccdf630bb1e32595aec49b2712f114d623f8
Didier Arcucci <didierarcucci@gmail.com>
2017-05-21 21:59:02 -0500

    start dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LocationStrategy, HashLocationStrategy, DatePipe } from '@angular/common';
 import { HttpModule } from '@angular/http';
 import { ModalModule } from 'ngx-bootstrap';
@@ -25,6 +26,7 @@ import { EstimateService } from './estimates/estimate.service';
 @NgModule({
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     BsDropdownModule.forRoot(),
     TabsModule.forRoot(),
---
+++ src/app/dashboard/mock-dashboard.ts
@@ -0,0 +1,14 @@
+export var dash = [
+    {
+        "name": "Hours YTD",
+        "value": 9318
+    },
+    {
+        "name": "Dollars YTD",
+        "value": 677000
+    },
+    {
+        "name": "Capitalization",
+        "value": 212000
+    }
+];
