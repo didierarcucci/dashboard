071ae3b4ae7d0cd21a21cdeb052e964d7e256fb1
Didier Arcucci <didierarcucci@gmail.com>
2017-05-24 20:39:04 -0500

    update charts

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,15 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 
-import { ytdprjhrs, yoyprjhrs, yoyprjdls } from './mock-dashboard';
+import { ytdprjhrs, yoyprjhrs, yoyprjdls, yoycapdls, yoyprjhrsrestyp, yoyprjdlsrestyp, yoycapbymo } from './mock-dashboard';
 
 @Component({
   templateUrl: 'dashboard.component.html'
 })
 export class DashboardComponent implements OnInit {
 
+  ytdprjhrs: any[];
   yoyprjhrs: any[];
   yoyprjdls: any[];
+  yoycapdls: any[];
+  yoyprjhrsrestyp: any[];
+  yoyprjdlsrestyp: any[];
+  yoycapbymo: any[];
 
   view: any[] = [700, 400];
 
@@ -26,16 +31,18 @@ export class DashboardComponent implements OnInit {
   /* colorScheme = {
     domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
   };
-
-  colorScheme = {
-    domain: ['#F44336', '#3F51B5', '#8BC34A', '#2196F3', '#009688', '#FF5722', '#CDDC39', '#00BCD4', '#FFC107', '#795548', '#607D8B']
-  }; */
+  */
 
   colorScheme = "ocean";
 
   constructor( ) {
+    Object.assign(this, {ytdprjhrs});
     Object.assign(this, {yoyprjhrs});
     Object.assign(this, {yoyprjdls});
+    Object.assign(this, {yoycapdls});
+    Object.assign(this, {yoyprjhrsrestyp});
+    Object.assign(this, {yoyprjdlsrestyp});
+    Object.assign(this, {yoycapbymo});
    }
 
    onSelect(event) {
--- src/app/dashboard/mock-dashboard.ts
+++ src/app/dashboard/mock-dashboard.ts
@@ -34,3 +34,144 @@ export var yoyprjdls = [
         value: 776000
     }
 ];
+
+export var yoycapdls = [
+    {
+        name: "2016 Actuals",
+        value: 264000
+    },
+    {
+        name: "2017 Actuals",
+        value: 232000
+    },
+    {
+        name: "2017 Budget",
+        value: 181000
+    }
+];
+
+export var yoyprjhrsrestyp = [
+    {
+        name: "2016",
+        series: [
+            {
+                name: "Employee",
+                value: 3177
+            },
+            {
+                name: "Contractor",
+                value: 6473
+            }
+        ]
+    },
+    {
+        name: "2017",
+        series: [
+            {
+                name: "Employee",
+                value: 2264
+            },
+            {
+                name: "Contractor",
+                value: 8309
+            }
+        ]
+    }
+];
+
+export var yoyprjdlsrestyp = [
+    {
+        name: "2016",
+        series: [
+            {
+                name: "Employee",
+                value: 298000
+            },
+            {
+                name: "Contractor",
+                value: 404000
+            }
+        ]
+    },
+    {
+        name: "2017",
+        series: [
+            {
+                name: "Employee",
+                value: 212000
+            },
+            {
+                name: "Contractor",
+                value: 564000
+            }
+        ]
+    }
+];
+
+export var yoycapbymo = [
+    {
+        name: "Jan",
+        series: [
+            {
+                name: "2016",
+                value: 54000
+            },
+            {
+                name: "2017",
+                value: 60000
+            }
+        ]
+    },
+    {
+        name: "Feb",
+        series: [
+            {
+                name: "2016",
+                value: 65000
+            },
+            {
+                name: "2017",
+                value: 52000
+            }
+        ]
+    },
+    {
+        name: "Mar",
+        series: [
+            {
+                name: "2016",
+                value: 60000
+            },
+            {
+                name: "2017",
+                value: 48000
+            }
+        ]
+    },
+    {
+        name: "Apr",
+        series: [
+            {
+                name: "2016",
+                value: 51000
+            },
+            {
+                name: "2017",
+                value: 45000
+            }
+        ]
+    },
+    {
+        name: "May",
+        series: [
+            {
+                name: "2016",
+                value: 35000
+            },
+            {
+                name: "2017",
+                value: 27000
+            }
+        ]
+    }
+];
